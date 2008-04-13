aaa43815edfd166713b2cdbea8b95f16645c8253
alexander.solovyov <devnull@localhost>
2008-04-13 15:02:04 +0000

    Refactoring client to work without configuration in settings, using kwargs in signal's connection

--- pingback/client.py
+++ pingback/client.py
@@ -2,14 +2,51 @@ import re
 from urlparse import urlsplit
 from xmlrpclib import ServerProxy, Fault
 from urllib2 import urlopen
+import socket
+import threading
 
 from BeautifulSoup import BeautifulSoup
 from django.contrib.sites.models import Site
+from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from pingback.models import PingbackClient, DirectoryPing
 from pingback.exceptions import PingbackNotConfigured, PingbackError
+
+
+class PingBackThread(threading.Thread):
+    def __init__(self, instance, url, links):
+        threading.Thread.__init__(self)
+        self.instance = instance
+        self.url = url
+        self.links = links
+
+    def run(self):
+        ctype = ContentType.objects.get_for_model(self.instance)
+        socket.setdefaulttimeout(10)
+        for link in self.links:
+            try:
+                PingbackClient.objects.get(url=link, content_type=ctype,
+                                           object_id=self.instance.id)
+            except PingbackClient.DoesNotExist:
+                pingback = PingbackClient(object=self.instance, url=link)
+                try:
+                    f = urlopen(link)
+                    server_url = f.info().get('X-Pingback', '') or \
+                                 search_link(f.read(512 * 1024))
+                    if server_url:
+                        server = ServerProxy(server_url)
+                        try:
+                            result = server.pingback.ping(self.url, link)
+                        except Exception, e:
+                            pingback.success = False
+                        else:
+                            pingback.success = not PingbackError.is_error(result)
+                except (IOError, ValueError, Fault), e:
+                    pass
+                pingback.save()
+        socket.setdefaulttimeout(None)
 
 
 def callab(smth):
@@ -19,93 +56,56 @@ def callab(smth):
         return smth
 
 
-class PingbackHelper:
-    """ Pingback client settings resolver.
-
-    Requires settings.PINGBACK_CLIENT in following format::
-
-        {
-        'some.model': {'content': 'body', 'url': 'get_absolute_url'},
-        'blog.post': {'content': 'html', 'url': 'get_another_url'},
-        }
-
-    Where:
-     - 'some.model' and 'blog.post' are 'app_name.model_name' in lowercase
-     - 'body' and 'html' are attributes, which contains HTML with links to ping. Can be callable.
-     - 'get_*_url' is attribute, which contains URL of object. Can be callable.
-
-    """
-    def __init__(self, instance):
-        self.instance = instance
-        opts = instance._meta
-        name = '%s.%s' % (opts.app_label, opts.module_name)
-        try:
-            conf = settings.PINGBACK_CLIENT[name]
-            self.content_attr = conf['content']
-            self.url_attr = conf['url']
-        except KeyError:
-            raise PingbackNotConfigured('Please configure PINGBACK_CLIENT for %s.' % name)
-
-    def get_content(self):
-        content = getattr(self.instance, self.content_attr, None)
-        return callab(content)
-
-    def get_url(self):
-        url = getattr(self.instance, self.url_attr, None)
-        return callab(url)
+def search_link(content):
+    match = re.search(r'<link rel="pingback" href="([^"]+)" ?/?>', content)
+    return match and match.group(1)
 
 
-def ping_external_links(instance):
+def ping_external_links(instance, content_attr, url_attr):
     """ Pingback client function.
+
+    Arguments::
+
+     - `instance` - object, which is the source for pingbacks
+     - `content_attr` - name of attribute, which contains content with links,
+       must be HTML. Can be callable.
+     - `url_attr` - name of attribute, which contains url of object. Can be
+       callable.
 
     Credits go to Ivan Sagalaev.
     """
     domain = Site.objects.get_current().domain
-    ph = PingbackHelper(instance)
-    content = ph.get_content()
-    url = 'http://%s%s' % (domain, ph.get_url())
+    content = callab(getattr(instance, content_attr))
+    url = 'http://%s%s' % (domain, callab(getattr(instance, url_attr)))
 
     def is_external(external, url):
         path_e = urlsplit(external)[2]
         path_i = urlsplit(url)[2]
         return path_e != path_i
 
-    def search_link(content):
-        match = re.search(r'<link rel="pingback" href="([^"]+)" ?/?>', content)
-        return match and match.group(1)
-
     soup = BeautifulSoup(content)
     links = [a['href'] for a in soup.findAll('a') if is_external(a['href'], url)]
 
-    # TODO: execute this code in the thread
-    for link in links:
-        if PingbackClient.objects.count_for_link(instance, link):
-            continue
-        pingback = PingbackClient(object=instance, url=link)
-        try:
-            f = urlopen(link)
-            server_url = f.info().get('X-Pingback', '') or search_link(f.read(512 * 1024))
-            if server_url:
-                server = ServerProxy(server_url)
-                try:
-                    result = server.pingback.ping(url, link)
-                except Exception, e:
-                    pingback.success = False
-                else:
-                    pingback.success = not PingbackError.is_error(result)
-        except (IOError, ValueError, Fault), e:
-            pass
-        pingback.save()
+    pbt = PingBackThread(instance=instance, url=url, links=links)
+    pbt.start()
 
-def ping_directories(instance):
-    """Ping blog directories"""
 
+def ping_directories(instance, content_attr, url_attr):
+    """Ping blog directories
+
+    Arguments::
+
+     - `instance` - object, which is the source for pingbacks
+     - `content_attr` - name of attribute, which contains content with links,
+       must be HTML. Can be callable.
+     - `url_attr` - name of attribute, which contains url of object. Can be
+       callable.
+    """
     domain = Site.objects.get_current().domain
-    ph = PingbackHelper(instance)
-    content = ph.get_content()
+    content = callab(getattr(instance, content_attr))
     blog_name = settings.BLOG_NAME
     blog_url = 'http://%s/' % domain
-    object_url = 'http://%s%s' % (domain, ph.get_url())
+    object_url = 'http://%s%s' % (domain, callab(getattr(instance, url_attr)))
     # TODO: cleanup generation of RSS feed and use it here instead of ATOM feed
     # because ATOM feed is not supported well by some ugly sites
     feed_url = 'http://%s%s' % (domain, reverse('atom_feed', args=['blog']))
