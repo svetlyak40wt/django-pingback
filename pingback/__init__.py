85f219373ab4c17305bc78b725e98adaf76db3b9
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-25 02:30:15 +0300

    Added middleware to set X-Pingback header. Added setup tools packaging script. Removed xmlrpc library, now this package depends on django-xmlrpc.

--- pingback/__init__.py
+++ pingback/__init__.py
@@ -18,6 +18,9 @@ from pingback.models import Pingback
 from pingback.exceptions import PingbackError
 from pingback.client import ping_external_links, ping_directories
 
+VERSION = (0, 1, 0)
+__version__ = '.'.join(map(str, VERSION))
+
 __all__ = ['Pingback', 'ping_external_links', 'ping_directories',
            'create_ping_func']
 
