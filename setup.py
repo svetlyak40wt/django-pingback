a36c2d4fa2109f35f51b83c22634d2c5c73d16e6
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-25 02:30:15 +0300

    Added middleware to set X-Pingback header. Added setup tools packaging script. Removed xmlrpc library, now this package depends on django-xmlrpc.
    committer: Alexander Artemenko <svetlyak.40wt@gmail.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-pingback',
+    version = __import__('pingback').__version__,
+    description = 'Pingback client/server implementation for Django framework.',
+    keywords = 'django apps',
+    license = 'New BSD License',
+    author = 'Alexander Solovyov',
+    author_email = 'piranha@piranha.org.ua',
+    maintainer = 'Alexander Artemenko',
+    maintainer_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://code.google.com/p/django-pingback/',
+    install_requires = ['django-xmlrpc>=0.1.0'],
+    dependency_links = ['http://pypi.aartemenko.com'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = find_packages(exclude=['xmlrpc']),
+    include_package_data = True,
+)
+
