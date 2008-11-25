bc45abafb2fd0b1d4bc927c23e5384f90287bc1d
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-25 04:59:00 +0300

    All code from 'pingback-connect-example.py' was updated and moved to the README.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-pingback',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Pingback client/server implementation for Django framework.',
     keywords = 'django apps',
     license = 'New BSD License',
